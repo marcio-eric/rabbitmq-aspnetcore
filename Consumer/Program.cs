652469b6efbe113dded762330392a73527e975f5
marcio-eric <marcio.eric.nicolau@gmail.com>
2020-01-28 10:54:36 -0300

    Entendido

---
+++ Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "testes",
+                Password = "Testes2018!"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare(queue: "TestesASPNETCore",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+            channel.BasicConsume(queue: "TestesASPNETCore",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.WriteLine("Aguardando mensagens para processamento");
+            Console.WriteLine("Pressione uma tecla para encerrar...");
+            Console.ReadKey();
+        }
+        
+        private static void Consumer_Received(
+            object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine(Environment.NewLine +
+                              "[Nova mensagem recebida] " + message);
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "testes",
+                Password = "Testes2018!"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "TestesASPNETCore",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var message = $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - Conteúdo da Mensagem: Menssagem";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                routingKey: "TestesASPNETCore",
+                basicProperties: null,
+                body: body);
+        }
+    }
+}
